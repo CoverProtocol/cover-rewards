c0272efea58fbcbc907ac5ab8a73ba5285661288
Pumpkin <cryptopumpkin2020@gmail.com>
2021-01-05 12:41:59 -0800

    setup dependencies

---
+++ contracts/BonusRewards.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: NONE
+
+pragma solidity ^0.8.0;
+
+import "./utils/Ownable.sol";
+import "./interfaces/IBonusRewards.sol";
+
+/**
+ * @title Cover Protocol Bonus Token Rewards contract
+ * @author crypto-pumpkin
+ */
+contract BonusRewards is Ownable {
+}
---
+++ contracts/interfaces/IBonusRewards.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: NONE
+
+pragma solidity ^0.8.0;
+
+/**
+ * @title Cover Protocol Bonus Token Rewards Interface
+ * @author crypto-pumpkin
+ */
+interface IBonusRewards {
+}
